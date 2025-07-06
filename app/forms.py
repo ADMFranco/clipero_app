018c63c33d3b5abaad53b3c23a8e21ad4ccedc85
Franco Matheus <matheusvargasorbegoso@gmail.com>
2025-07-05 20:59:10 -0500

    Implementando Login y panel para cliperos

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms.validators import ValidationError
+from .models import Usuario
 
 class RegistroForm(FlaskForm):
     nombre = StringField('Nombre', validators=[DataRequired(), Length(min=2, max=100)])
@@ -11,3 +13,8 @@ class RegistroForm(FlaskForm):
     contraseña = PasswordField('Contraseña', validators=[DataRequired(), Length(min=6)])
     confirmar = PasswordField('Confirmar contraseña', validators=[DataRequired(), EqualTo('contraseña')])
     submit = SubmitField('Registrarse')
+
+class LoginForm(FlaskForm):
+    correo = StringField('Correo', validators=[DataRequired(), Email()])
+    contraseña = PasswordField('Contraseña', validators=[DataRequired()])
+    submit = SubmitField('Iniciar Sesión')
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,9 @@ from .forms import RegistroForm
 from .models import Usuario
 from . import db
 from werkzeug.security import generate_password_hash
+from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.security import check_password_hash
+from .forms import LoginForm
 
 main = Blueprint('main', __name__)
 
@@ -27,3 +30,28 @@ def registro():
         flash('¡Registro exitoso! Ya puedes iniciar sesión.', 'success')
         return redirect(url_for('main.index'))
     return render_template('registro.html', form=form)
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        usuario = Usuario.query.filter_by(correo=form.correo.data).first()
+        if usuario and check_password_hash(usuario.contraseña, form.contraseña.data):
+            login_user(usuario)
+            flash('¡Has iniciado sesión correctamente!', 'success')
+            return redirect(url_for('main.panel'))
+        else:
+            flash('Correo o contraseña incorrectos', 'danger')
+    return render_template('login.html', form=form)
+
+@main.route('/panel')
+@login_required
+def panel():
+    return f"Bienvenido {current_user.nombre}, este es tu panel de clipero."
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Has cerrado sesión', 'info')
+    return redirect(url_for('main.index'))
